017b3d0809f9f115486dd1dbbb90d2652470358a
Megan Sundheim <meggsatron@wireless-10-146-90-244.public.utexas.edu>
2024-03-06 17:21:37 -0600

    Started creating Geo-Challenge screens in their own XCode project. When changing tab icons, XCode kept crashing.

---
+++ Geo-Challenges/Geo-Challenges/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Geo-Challenges
+//
+//  Created by Megan Sundheim on 3/6/24.
+//
+
+import UIKit
+
+class  MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
