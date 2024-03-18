f63e5bed5e55c240be0f255aa017bc460526412b
Brandon Ling <brandonling2001@gmail.com>
2024-03-18 18:25:10 -0500

    buttons

--- TimeFrame/HomeScreenVCs/AlbumViewController.swift
+++ TimeFrame/HomeScreenVCs/AlbumViewController.swift
@@ -10,44 +10,76 @@ import FirebaseAuth
 import FirebaseDatabase
 
 class AlbumViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-
     
-    @IBOutlet weak var settings: UIBarButtonItem!
-    @IBOutlet weak var newItem: UIBarButtonItem!
     var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setCustomBackImage()
-    }
-    
-    override func didMove(toParent parent: UIViewController?) {
-        if !(parent?.isEqual(self.parent) ?? false) {
-            print("Parent view loaded")
+            
+        // Set up navigation bar
+        title = "Album"
+        let addPhotoMenuItem = UIAction(title: "Add Photo", image: UIImage(systemName: "plus")) { _ in
+            // Add Photo action
+            self.addPhoto()
         }
-        super.didMove(toParent: parent)
+        
+        let createTimeframeMenuItem = UIAction(title: "Create Timeframe", image: UIImage(systemName: "plus")) { _ in
+            // Create Timeframe action
+            self.createTimeframe()
+        }
+        
+        let renameAlbumMenuItem = UIAction(title: "Rename Album", image: UIImage(systemName: "pencil")) { _ in
+            // Rename Album action
+            self.renameAlbum()
+        }
+        
+        let deleteAlbumMenuItem = UIAction(title: "Delete Album", image: UIImage(systemName: "trash"), attributes: .destructive) { _ in
+            // Delete Album action
+            self.deleteAlbum()
+        }
+            
+        let menu = UIMenu(title: "Album Menu", children: [addPhotoMenuItem, createTimeframeMenuItem, renameAlbumMenuItem, deleteAlbumMenuItem])
+        
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Menu", image: UIImage(systemName: "ellipsis.circle"), primaryAction: nil, menu: menu)
+        }
+        
+    func addPhoto() {
+        // Handle Add Photo action
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+        
+    func createTimeframe() {
+        // Handle Create Timeframe action
+        print("Create Timeframe")
+    }
+        
+    func renameAlbum() {
+        // Handle Rename Album action
+        print("Rename Album")
+    }
+        
+    func deleteAlbum() {
+        // Handle Delete Album action
+        print("Delete Album")
     }
     
-    @IBAction func addItemPressed(_ sender: Any) {
-        let controller = UIAlertController(
-            title: nil,
-            message: nil,
-            preferredStyle: .actionSheet
-        )
-        controller.addAction(
-            UIAlertAction(title: "Take Picture", style: .default) { action in
-                self.imagePicker.sourceType = .camera
-                self.imagePicker.cameraFlashMode = .off
-                self.present(self.imagePicker, animated: true, completion: nil)
-            }
-        )
-        controller.addAction(
-            UIAlertAction(title: "Create TimeFrame", style: .default) { action in
-                print("TimeFrame")
-            }
-        )
-        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        present(controller, animated: true)
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+        // Use the captured photo
+        // Here you can save the image or use it as required
+        // You might want to present a confirmation dialog to the user before saving the image
+        // For now, we just dismiss the image picker
+        dismiss(animated: true, completion: nil)
+        }
     }
+        
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // User cancelled the image picker
+        dismiss(animated: true, completion: nil)
+    }
+    
     
 }
