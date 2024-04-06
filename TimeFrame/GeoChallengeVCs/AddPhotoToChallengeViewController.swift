7d42bcc3889876524178fb4d37264b46100bd237
Megan Sundheim <msundheim03@gmail.com>
2024-04-06 14:36:51 -0500

    Implemented camera overlay in Geo-Challenges.

--- TimeFrame/GeoChallengeVCs/AddPhotoToChallengeViewController.swift
+++ TimeFrame/GeoChallengeVCs/AddPhotoToChallengeViewController.swift
@@ -9,11 +9,100 @@
 // Course: CS371L
 
 import UIKit
+import AVFoundation
 
-class AddPhotoToChallengeViewController: UIViewController {
+class AddPhotoToChallengeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var previewView: UIImageView!
+    
+    var overlayView = UIImageView(image: UIImage(named: "InitialImagePlaceholder")) // TODO: replace
+    let picker = UIImagePickerController()
+    var cameraLoaded = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        picker.delegate = self
         setCustomBackImage()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !cameraLoaded {
+            // Only load the camera once automatically.
+            showCamera() // TODO: need to load new overlay image
+        } else if previewView.image == nil {
+            performSegue(withIdentifier: "CameraToTimeLapseSegue", sender: nil)
+        }
+    }
+    
+    func showCamera() {
+        // Check availability of camera.
+        if UIImagePickerController.availableCaptureModes(for: .rear) != nil {
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video) {
+                    accessGranted in guard accessGranted == true
+                    else {return}
+                }
+            case .authorized:
+                break
+            default:
+                print("Access denied.") //TODO: show some error and segue if access denied
+                return
+            }
+            
+            cameraLoaded = true
+            picker.sourceType = .camera
+            picker.allowsEditing = false //TODO: add and delete photos from firebase
+            picker.cameraCaptureMode = .photo //TODO: add overlay code to home screen
+            overlayView.alpha = 0.4
+            
+            // Get bounds for camera preview.
+            let screenSize = UIScreen.main.bounds.size
+            let ratio: CGFloat = 4.0 / 3.0
+            let cameraHeight: CGFloat = screenSize.width * ratio
+            overlayView.frame = CGRect(x: 0, y: 121.5, width: screenSize.width, height: cameraHeight)
+            
+            overlayView.contentMode = .scaleAspectFill
+            picker.cameraOverlayView = overlayView
+            
+            // Add observer to the user capturing a photo.
+            NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "_UIImagePickerControllerUserDidCaptureItem"), object:nil, queue:nil, using: { note in
+                // Remove overlay.
+                self.overlayView.alpha = 0
+                self.picker.cameraOverlayView = self.overlayView
+            })
+            
+            self.present(picker, animated: true, completion: nil)
+        } else {
+            // Not available, pop up an alert.
+            let alertVC = UIAlertController(title: "No Camera Available", message: "Sorry, this device does not have a camera.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertVC.addAction(okAction)
+            present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let chosenImage = info[.originalImage] as! UIImage
+        
+        // Shrink to visible size.
+        previewView.contentMode = .scaleAspectFit
+        
+        // Put the image in the imageView.
+        previewView.image = chosenImage
+        
+        // Dismiss this popover.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onRetakePhotoButtonPressed(_ sender: Any) {
+        // Retake photo.
+        showCamera()
+        //TODO: delete old photo and save new photo
     }
 }
--- TimeFrame/GeoChallengeVCs/ChallengeCameraViewController.swift
+++ TimeFrame/GeoChallengeVCs/ChallengeCameraViewController.swift
@@ -30,6 +30,9 @@ class ChallengeCameraViewController: UIViewController {
         return button
     }()
     
+    // Image taken by camera.
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
@@ -99,6 +102,10 @@ class ChallengeCameraViewController: UIViewController {
     @objc private func didTapTakePhoto() {
         // TODO: can change camera settings here
         output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+
+        let addPhotoVC = AddPhotoToChallengeViewController()
+        addPhotoVC.modalPresentationStyle = .fullScreen
+        self.present(addPhotoVC, animated: true, completion: nil)
     }
 }
 
@@ -111,10 +118,132 @@ extension ChallengeCameraViewController : AVCapturePhotoCaptureDelegate {
         // Stop running the camera.
         session?.stopRunning()
         
-        let image = UIImage(data: data)
-        let imageView = UIImageView(image: image)
-        imageView.contentMode = .scaleAspectFill
-        imageView.frame = view.bounds
-        view.addSubview(imageView)
+        self.image = UIImage(data: data)
     }
 }
+
+
+////
+////  ChallengeCameraViewController.swift
+////  TimeFrame
+////
+////  Created by Megan Sundheim on 3/16/24.
+////
+//// Project: TimeFrame
+//// EID: mas23586
+//// Course: CS371L
+//
+//import AVFoundation
+//import UIKit
+//
+//class ChallengeCameraViewController: UIViewController {
+//    // Capture session.
+//    var session: AVCaptureSession?
+//    
+//    // Photo output.
+//    let output = AVCapturePhotoOutput()
+//    
+//    // Video preview.
+//    let previewLayer = AVCaptureVideoPreviewLayer()
+//    
+//    // Shutter button.
+//    private let shutterButton: UIButton = {
+//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+//        button.layer.cornerRadius = 50
+//        button.layer.borderWidth = 10
+//        button.layer.borderColor = UIColor.white.cgColor
+//        return button
+//    }()
+//    
+//    // Image taken by camera.
+//    var image: UIImage?
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .black
+//        previewLayer.backgroundColor = UIColor.systemRed.cgColor
+//        view.layer.addSublayer(previewLayer)
+//        view.addSubview(shutterButton)
+//        
+//        checkCameraPermissions()
+//        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
+//        setCustomBackImage()
+//    }
+//    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        previewLayer.frame = view.bounds
+//        shutterButton.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height - 100)
+//    }
+//    
+//    private func checkCameraPermissions() {
+//        switch AVCaptureDevice.authorizationStatus(for: .video) {
+//        case .notDetermined:
+//            // Request permission.
+//            AVCaptureDevice.requestAccess(for: .video) { granted in
+//                guard granted else {
+//                    return
+//                }
+//                
+//                DispatchQueue.main.async {
+//                    // Updating the UI.
+//                    self.setUpCamera()
+//                }
+//            }
+//        case .restricted:
+//            break
+//        case .denied:
+//            break
+//        case .authorized:
+//            setUpCamera()
+//        @unknown default:
+//            break
+//        }
+//    }
+//    
+//    private func setUpCamera() {
+//        let session = AVCaptureSession()
+//        if let device = AVCaptureDevice.default(for: .video) {
+//            do {
+//                let input = try AVCaptureDeviceInput(device: device)
+//                if session.canAddInput(input) {
+//                    session.addInput(input)
+//                }
+//                
+//                if session.canAddOutput(output) {
+//                    session.addOutput(output)
+//                }
+//                
+//                previewLayer.videoGravity = .resizeAspectFill // Make sure camera feed is not distorted.
+//                previewLayer.session = session
+//                session.startRunning()
+//                self.session = session
+//            } catch {
+//                print(error)
+//            }
+//        }
+//    }
+//    
+//    @objc private func didTapTakePhoto() {
+//        // TODO: can change camera settings here
+//        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+//
+//        let addPhotoVC = AddPhotoToChallengeViewController()
+//        addPhotoVC.modalPresentationStyle = .fullScreen
+//        self.present(addPhotoVC, animated: true, completion: nil)
+//    }
+//}
+//
+//extension ChallengeCameraViewController : AVCapturePhotoCaptureDelegate {
+//    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: (any Error)?) {
+//        guard let data = photo.fileDataRepresentation() else {
+//            return
+//        }
+//        
+//        // Stop running the camera.
+//        session?.stopRunning()
+//        
+//        self.image = UIImage(data: data)
+//    }
+//}
+//
