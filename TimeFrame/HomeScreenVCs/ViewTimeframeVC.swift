c678129f5672e1b315e3628b782b580a4ef478b2
katezhang1234 <kate.a.zhang@gmail.com>
2024-04-23 16:57:45 -0500

    Fixed issue with only 1 Timeframe showing, not multiple timeframes are stored along with their names

--- TimeFrame/HomeScreenVCs/ViewTimeframeVC.swift
+++ TimeFrame/HomeScreenVCs/ViewTimeframeVC.swift
@@ -1,44 +1,75 @@
-//
-//  OpenTimeframeVC.swift
+//  ViewTimeFrameVC.swift
 //  TimeFrame
 //
-//  Created by Kate Zhang on 4/8/24.
+//  Created by Kate Zhang on 4/1/24.
 //
 //  Project: TimeFrame
 //  EID: kz4696
 //  Course: CS371L
 
 
+import Foundation
 import UIKit
+import UniformTypeIdentifiers
 
-class OpenTimeframeVC: UIViewController {
+class ViewTimeframeVC: UIViewController {
+    var timeframeName: String!
+    var isPublic: Bool!
+    var isFavorite: Bool!
+    var isReversed: Bool!
+    var selectedDate: String!
+    var selectedSpeed: String!
+    var selectedPhotos: [UIImage]!
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var shareButton: UIButton!
-    var timeframeName: String!
-    var timeframeImages: [UIImage]!
+    var imageDuration: Float!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setCustomBackImage()
+        self.title = timeframeName
         shareButton.layer.cornerRadius = 5
-        timeframeImages = allTimeframes[timeframeName]
-        // Placeholder: sets all TimeFrames to have a static duration of 2.0 seconds for 1 iteration through all photos in the TimeFrame
-        imageView.animationDuration = 2.0
-        imageView.animationImages = timeframeImages
+        animateImage()
+    }
+    
+    func animateImage() {
+        if isReversed {
+            selectedPhotos = selectedPhotos.reversed()
+        }
+        let speedComponents = selectedSpeed.components(separatedBy: " ")
+        let actualSpeed = Float(speedComponents[0])!
+        let gifDuration = Float(selectedPhotos.count) / actualSpeed
+        // Calculates the duration for each image
+        imageDuration = gifDuration / Float(selectedPhotos.count)
+        imageView.animationDuration = TimeInterval(gifDuration)
+        imageView.animationImages = selectedPhotos
         imageView.startAnimating()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        imageView.animationImages = nil   // Release the memory from animating images
+    }
+    
+    // Adds the TimeFrame to the local array of all created TimeFrames and unwind segues back to the main home screen
+    @IBAction func onSaveTapped(_ sender: UIBarButtonItem) {
+        allTimeframes[timeframeName] = selectedPhotos
+        timeframeNames.append(timeframeName)
+        timeframeNames = timeframeNames.sorted()
+        performSegue(withIdentifier: "unwindViewTimeframeToHome", sender: self)
+    }
+    
     // Shares the TimeFrame as a GIF to the user's other apps when the "Share" button is tapped
-    @IBAction func onShareTapped(_ sender: Any) {
+    @IBAction func onShareTapped(_ sender: UIButton) {
         var shareItem: Any = ""
-        let gifURL = UIImage.animatedGif(from: timeframeImages, from: 2.0/Float(timeframeImages.count))
+        let gifURL = UIImage.animatedGif(from: selectedPhotos, from: imageDuration)
         
         if gifURL != nil {
             shareItem = gifURL!
         } else {
             // Share first photo in array if there is an error generating the GIF
-            shareItem = timeframeImages[0]
+            shareItem = selectedPhotos[0]
         }
         let activityController = UIActivityViewController(activityItems: [shareItem], applicationActivities: nil)
         present(activityController, animated: true, completion: nil)
